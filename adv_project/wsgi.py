3d84db7b254ff5b3363c86ec2664a3db514867bb
Rob Hamilton <RobHamilton3rd@gmail.com>
2020-03-03 11:00:01 -0500

    comment

--- adv_project/wsgi.py
+++ adv_project/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-#comment
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adv_project.settings')
 
 application = get_wsgi_application()
-
-x="hi"
