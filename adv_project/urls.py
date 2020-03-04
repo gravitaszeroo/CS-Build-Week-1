18f2e6fffa1711015624a9f40250033f88d79eed
Rob Hamilton <RobHamilton3rd@gmail.com>
2020-03-04 13:38:16 -0500

    Updated Pip File and front end dummy pages

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
 
+
+#importing from the views.py file
+from .views import (
+    home_page,
+    registration_page,
+    login_page
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('', home_page),
+    path('login/', login_page),
+    path('registration/', registration_page),
 ]
