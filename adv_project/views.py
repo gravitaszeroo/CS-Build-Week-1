edb58ec0b20702d279585d1862bc56f0a00a6c12
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-03-05 13:30:36 -0800

    pep8

--- adv_project/views.py
+++ adv_project/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 def home_page(request):
     return render(request, "hello_world.html")
 
+
 def registration_page(request):
     return HttpResponse("<h1>This is the Registration Page!</h1>")
+
 
 def login_page(request):
     return HttpResponse("<h1>This is the login page!</h1>")
