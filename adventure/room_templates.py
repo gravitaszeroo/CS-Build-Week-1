bc15fa5191226333a6949db9f1fe2bf6d65e825a
Coop Williams <ken.c.williams.iii@gmail.com>
2020-03-05 13:15:41 -0500

    remove template doors

--- adventure/room_templates.py
+++ adventure/room_templates.py
@@ -24,29 +24,6 @@ basic = [['█' for x in range(MAP_WIDTH)]] \
               for y in range(MAP_HEIGHT-2)] \
         + [['█' for x in range(MAP_WIDTH)]] \
 
-room_array = [
-    ['`' for x in range(MAP_WIDTH)]
-    for y in range(MAP_HEIGHT)
-]
-for pos, i in enumerate(room_array[0]):
-    room_array[0][pos] = '█'
-for pos, i in enumerate(room_array[-1]):
-    room_array[-1][pos] = '█'
-for pos, i in enumerate(room_array):
-    room_array[pos][0] = '█'
-    room_array[pos][-1] = '█'
-
-# █
-# test wall
-room_array[0][60] = '█'
-room_array[1][60] = '█'
-room_array[2][60] = '█'
-# test doors
-room_array[0][51] = 'n'
-room_array[2][48] = 'w'
-room_array[2][52] = 'e'
-room_array[4][51] = 's'
-
 # Top-left corner is 0,0
 default_array = [
     ['`' for x in range(MAP_WIDTH)]
@@ -64,12 +41,6 @@ for pos, i in enumerate(default_array[-1]):
 for pos, i in enumerate(default_array):
     default_array[pos][0] = '█'
     default_array[pos][-1] = '█'
-
-# Doors
-default_array[MAP_HEIGHT//2 - 2][MAP_WIDTH//2] = 'n'
-default_array[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 's'
-default_array[MAP_HEIGHT//2][MAP_WIDTH//2 + 2] = 'e'
-default_array[MAP_HEIGHT//2][MAP_WIDTH//2 - 2] = 'w'
 
 
 '''
@@ -95,12 +66,6 @@ center_void_array[MAP_HEIGHT//2-1][MAP_WIDTH//2-1] = '▓'
 center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2-1] = '▓'
 center_void_array[MAP_HEIGHT//2-1][MAP_WIDTH//2] = '▓'
 center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2] = '▓'
-
-# Doors
-center_void_array[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 'n'
-center_void_array[MAP_HEIGHT//2 - 2][MAP_WIDTH//2] = 's'
-center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2 + 2] = 'e'
-center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2 - 2] = 'w'
 
 '''
 void x axis
@@ -130,12 +95,6 @@ for pos, i in enumerate(x_axis_void[27]):
     if x_axis_void[MAP_HEIGHT//2][pos] in EMPTY_CHARS:
         x_axis_void[MAP_HEIGHT//2][pos] = '▓'
 
-# Doors
-x_axis_void[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 'n'
-x_axis_void[MAP_HEIGHT//2 - 2][MAP_WIDTH//2] = 's'
-x_axis_void[MAP_HEIGHT//2][MAP_WIDTH//2 + 2] = 'e'
-x_axis_void[MAP_HEIGHT//2][MAP_WIDTH//2 - 2] = 'w'
-
 '''
 void y axis
 '''
@@ -163,12 +122,6 @@ for pos, i in enumerate(y_axis_void):
         y_axis_void[pos][MAP_WIDTH//2-1] = '▓'
     if y_axis_void[pos][MAP_WIDTH//2] in EMPTY_CHARS:
         y_axis_void[pos][MAP_WIDTH//2] = '▓'
-
-# Doors
-y_axis_void[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 'n'
-y_axis_void[MAP_HEIGHT//2 - 2][MAP_WIDTH//2] = 's'
-y_axis_void[MAP_HEIGHT//2][MAP_WIDTH//2 + 2] = 'e'
-y_axis_void[MAP_HEIGHT//2][MAP_WIDTH//2 - 2] = 'w'
 
 
 # list of rooms
