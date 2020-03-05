b03b9e9a123eb4f1cdc7a33c8d48b7b087bbd347
Coop Williams <ken.c.williams.iii@gmail.com>
2020-03-05 12:03:07 -0500

    change void char to darker char

--- adventure/room_templates.py
+++ adventure/room_templates.py
@@ -8,7 +8,7 @@ MAP_HEIGHT = 28
 
 # Tilesets
 # Tiles which block movement
-BLOCKED_CHARS = ['X', '█', ' ', 'O']
+BLOCKED_CHARS = ['X', '█', ' ', '▓']
 # Tiles which allow movement
 EMPTY_CHARS = ['`']
 # Tiles which transport you to another room when entered
@@ -91,10 +91,10 @@ for pos, i in enumerate(center_void_array):
     center_void_array[pos][-1] = '█'
 
 # Create a 2x2 center void
-center_void_array[MAP_HEIGHT//2-1][MAP_WIDTH//2-1] = 'O'
-center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2-1] = 'O'
-center_void_array[MAP_HEIGHT//2-1][MAP_WIDTH//2] = 'O'
-center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2] = 'O'
+center_void_array[MAP_HEIGHT//2-1][MAP_WIDTH//2-1] = '▓'
+center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2-1] = '▓'
+center_void_array[MAP_HEIGHT//2-1][MAP_WIDTH//2] = '▓'
+center_void_array[MAP_HEIGHT//2][MAP_WIDTH//2] = '▓'
 
 # Doors
 center_void_array[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 'n'
@@ -126,9 +126,9 @@ for pos, i in enumerate(x_axis_void):
 # Create void splitting room along center across x axis
 for pos, i in enumerate(x_axis_void[27]):
     if x_axis_void[MAP_HEIGHT//2-1][pos] in EMPTY_CHARS:
-        x_axis_void[MAP_HEIGHT//2-1][pos] = 'O'
+        x_axis_void[MAP_HEIGHT//2-1][pos] = '▓'
     if x_axis_void[MAP_HEIGHT//2][pos] in EMPTY_CHARS:
-        x_axis_void[MAP_HEIGHT//2][pos] = 'O'
+        x_axis_void[MAP_HEIGHT//2][pos] = '▓'
 
 # Doors
 x_axis_void[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 'n'
@@ -160,9 +160,9 @@ for pos, i in enumerate(y_axis_void):
 # Create void splitting room along center across y axis
 for pos, i in enumerate(y_axis_void):
     if y_axis_void[pos][MAP_WIDTH//2-1] in EMPTY_CHARS:
-        y_axis_void[pos][MAP_WIDTH//2-1] = 'O'
+        y_axis_void[pos][MAP_WIDTH//2-1] = '▓'
     if y_axis_void[pos][MAP_WIDTH//2] in EMPTY_CHARS:
-        y_axis_void[pos][MAP_WIDTH//2] = 'O'
+        y_axis_void[pos][MAP_WIDTH//2] = '▓'
 
 # Doors
 y_axis_void[MAP_HEIGHT//2 + 2][MAP_WIDTH//2] = 'n'
