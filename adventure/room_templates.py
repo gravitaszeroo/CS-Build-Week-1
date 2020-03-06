a33a0dd6882500274023e48ad1cb4d8dc0f82932
Coop Williams <ken.c.williams.iii@gmail.com>
2020-03-06 10:03:08 -0500

    hide players in grass

--- adventure/room_templates.py
+++ adventure/room_templates.py
@@ -15,7 +15,7 @@ EMPTY_CHARS = ['`']
 # Tiles which transport you to another room when entered
 DOOR_CHARS = ['n', 's', 'e', 'w']
 # Tiles which block LoS
-HIDDEN_CHARS = ['f']
+HIDDEN_CHARS = ['ƒ', 'l', 'r']
 ITEM_CHARS = ['$','€','¥']
 '''
 Default room creation
